c9d444ca373bed5a8e97cb66cc2adfc3ba262231
Almitha Joju <almithajoju@gmail.com>
2022-09-26 10:10:38 +0200

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
+  const [removedToDos, setRemovedToDos] = useState([]);
+
+  const time = new Date().toLocaleTimeString();
+  var d = new Date();
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const addTodos = (e) => {
+    setToDos([...toDos, { id: Date.now(), Text: toDo, status: false }])
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <div className="mainHeading">
+      <h1>ToDo List</h1>
     </div>
+    <div className="subHeading">
+      <br />
+      <h2>Whoop, it's {days[d.getDay()]} 🌝 ☕ </h2>
+    </div>
+    <div className="input">
+      <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+      <i  onClick={()=>setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
+    </div>
+    <div className="todos">
+      { toDos.map((obj)=>{
+        return(
+      <div className="todo">
+        <div className="left">
+          <input onChange={(e)=>{
+            //console.log(e.target.checked)
+            //console.log(obj)
+            setToDos(toDos.filter(obj2=>{
+              if(obj2.id===obj.id){
+                obj2.status=e.target.checked
+              }
+              return obj2
+            }))
+          }} value={obj.status} type="checkbox" name="" id="" />
+          <p>{obj.text}</p>
+        </div>
+        <div className="right">
+          <i  onClick={() => {
+                  if (window.confirm("Are you sure to remove this ?")) {
+                    setRemovedToDos((removedToDos) => [...removedToDos, obj]);
+                    setToDos(toDos.filter(obj2 => {
+                      if (obj2.id !== obj.id) {
+                        return obj2;
+                      }
+                      return null;
+                    }))
+                  }
+                }} className="fas fa-times" ></i>
+          
+        </div>
+      </div>)
+      }) }
+    </div>
+  </div>
   );
 }
 
